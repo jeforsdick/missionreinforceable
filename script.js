3e8acd0752ef903af7f3108219590d62c93d6e49
jeforsdick <jess.olson@utah.edu>
2025-08-27 21:56:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ async function sendResultsIfNeeded() {
         headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
         body: json
       });
-      setStatus('Results sent.');
+      setStatus('Thank you');
     }
   } catch (err) {
     setStatus('Could not send results: ' + (err?.message || String(err)));
